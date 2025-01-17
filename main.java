1b343ce988f24af5e35bb47fe508a3739eab14d0
Kimberly Elikem Hatsu <hatsukimberly5@gmail.com>
2025-01-17 10:36:49 +0000

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,26 @@
-assignment
+ public class Main {
+    public static void main(String[]args){
+        CommissionEmployee employee = new CommissionEmployee("Kimberly", "Hatsu", "123456789", 10000.00, 0.5);
+        System.out.println(employee);
+
+        employee.setGrossSales(15000.00);
+        employee.setCommissionRate(0.6);
+        
+        System.out.println("\nUpdated Employee Details:");
+        System.out.println(employee);
+
+        System.out.printf("%nEarnings: $%.2f%n", employee.earnings());
+
+        try {
+            employee.setGrossSales(-5000.00);
+        } catch (IllegalArgumentException e) {
+            System.out.printf("%nException: %s%n", e.getMessage());
+        }
+
+        try {
+            employee.setCommissionRate(1.5);
+        } catch (IllegalArgumentException e) {
+            System.out.printf("%nException: %s%n", e.getMessage());
+        }
+    }
+}
